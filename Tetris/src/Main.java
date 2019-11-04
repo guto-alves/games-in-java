681a5b58bf388f3e4a166ec63a2f33e04c6379a3
guto-alves <gustavo.almeida13@fatec.sp.gov.br>
2019-11-03 22:49:43 -0200

    Change frame title

--- Tetris/src/Main.java
+++ Tetris/src/Main.java
@@ -5,7 +5,7 @@ import javax.swing.JFrame;
 public class Main {
 
 	public static void main(String[] args) {
-		JFrame frame = new JFrame("The Game!");
+		JFrame frame = new JFrame("Tetris");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		frame.add(new Game(frame));
